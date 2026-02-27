6dc7db2437a6fcef2876de641d2587628f8f66e4
dcchill <dariancsmart@gmail.com>
2026-02-27 13:51:29 -0500

    MCreator Merge commit '019ea8fcbdb07819351043591c830fc2fc2490f7' into d5a3750a37867b6557a49163077a9c804f4c51ac

--- src/main/java/net/gearbound/init/GearboundModBlockEntities.java
+++ src/main/java/net/gearbound/init/GearboundModBlockEntities.java
@@ -1,6 +1,6 @@
 /*
-*    MCreator note: This file will be REGENERATED on each build.
-*/
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
 package net.gearbound.init;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -23,25 +23,8 @@ import net.gearbound.GearboundMod;
 public class GearboundModBlockEntities {
 	public static final DeferredRegister<BlockEntityType<?>> REGISTRY = DeferredRegister.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, GearboundMod.MODID);
 	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> BROWN_BRASS_BACKPACK = register("brown_brass_backpack", GearboundModBlocks.BROWN_BRASS_BACKPACK, BrownBrassBackpackBlockEntity::new);
-	// Start of user code block custom block entities
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> WHITE_BRASS_BACKPACK = register("white_brass_backpack", GearboundModBlocks.WHITE_BRASS_BACKPACK, net.gearbound.block.entity.WhiteBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> LIGHT_GRAY_BRASS_BACKPACK = register("light_gray_brass_backpack", GearboundModBlocks.LIGHT_GRAY_BRASS_BACKPACK,
-			net.gearbound.block.entity.LightGrayBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> GRAY_BRASS_BACKPACK = register("gray_brass_backpack", GearboundModBlocks.GRAY_BRASS_BACKPACK, net.gearbound.block.entity.GrayBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> BLACK_BRASS_BACKPACK = register("black_brass_backpack", GearboundModBlocks.BLACK_BRASS_BACKPACK, net.gearbound.block.entity.BlackBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> RED_BRASS_BACKPACK = register("red_brass_backpack", GearboundModBlocks.RED_BRASS_BACKPACK, net.gearbound.block.entity.RedBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> ORANGE_BRASS_BACKPACK = register("orange_brass_backpack", GearboundModBlocks.ORANGE_BRASS_BACKPACK, net.gearbound.block.entity.OrangeBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> YELLOW_BRASS_BACKPACK = register("yellow_brass_backpack", GearboundModBlocks.YELLOW_BRASS_BACKPACK, net.gearbound.block.entity.YellowBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> LIME_BRASS_BACKPACK = register("lime_brass_backpack", GearboundModBlocks.LIME_BRASS_BACKPACK, net.gearbound.block.entity.LimeBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> GREEN_BRASS_BACKPACK = register("green_brass_backpack", GearboundModBlocks.GREEN_BRASS_BACKPACK, net.gearbound.block.entity.GreenBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> CYAN_BRASS_BACKPACK = register("cyan_brass_backpack", GearboundModBlocks.CYAN_BRASS_BACKPACK, net.gearbound.block.entity.CyanBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> LIGHT_BLUE_BRASS_BACKPACK = register("light_blue_brass_backpack", GearboundModBlocks.LIGHT_BLUE_BRASS_BACKPACK,
-			net.gearbound.block.entity.LightBlueBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> BLUE_BRASS_BACKPACK = register("blue_brass_backpack", GearboundModBlocks.BLUE_BRASS_BACKPACK, net.gearbound.block.entity.BlueBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> PURPLE_BRASS_BACKPACK = register("purple_brass_backpack", GearboundModBlocks.PURPLE_BRASS_BACKPACK, net.gearbound.block.entity.PurpleBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> MAGENTA_BRASS_BACKPACK = register("magenta_brass_backpack", GearboundModBlocks.MAGENTA_BRASS_BACKPACK, net.gearbound.block.entity.MagentaBrassBackpackBlockEntity::new);
-	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> PINK_BRASS_BACKPACK = register("pink_brass_backpack", GearboundModBlocks.PINK_BRASS_BACKPACK, net.gearbound.block.entity.PinkBrassBackpackBlockEntity::new);
 
+	// Start of user code block custom block entities
 	// End of user code block custom block entities
 	private static DeferredHolder<BlockEntityType<?>, BlockEntityType<?>> register(String registryname, DeferredHolder<Block, Block> block, BlockEntityType.BlockEntitySupplier<?> supplier) {
 		return REGISTRY.register(registryname, () -> BlockEntityType.Builder.of(supplier, block.get()).build(null));
--- src/main/java/net/gearbound/init/GearboundModBlocks.java
+++ src/main/java/net/gearbound/init/GearboundModBlocks.java
@@ -1,6 +1,6 @@
 /*
-*    MCreator note: This file will be REGENERATED on each build.
-*/
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
 package net.gearbound.init;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -15,20 +15,5 @@ public class GearboundModBlocks {
 	public static final DeferredRegister.Blocks REGISTRY = DeferredRegister.createBlocks(GearboundMod.MODID);
 	public static final DeferredBlock<Block> BROWN_BRASS_BACKPACK = REGISTRY.register("brown_brass_backpack", BrownBrassBackpackBlock::new);
 	// Start of user code block custom blocks
-	public static final DeferredBlock<Block> WHITE_BRASS_BACKPACK = REGISTRY.register("white_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> LIGHT_GRAY_BRASS_BACKPACK = REGISTRY.register("light_gray_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> GRAY_BRASS_BACKPACK = REGISTRY.register("gray_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> BLACK_BRASS_BACKPACK = REGISTRY.register("black_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> RED_BRASS_BACKPACK = REGISTRY.register("red_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> ORANGE_BRASS_BACKPACK = REGISTRY.register("orange_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> YELLOW_BRASS_BACKPACK = REGISTRY.register("yellow_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> LIME_BRASS_BACKPACK = REGISTRY.register("lime_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> GREEN_BRASS_BACKPACK = REGISTRY.register("green_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> CYAN_BRASS_BACKPACK = REGISTRY.register("cyan_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> LIGHT_BLUE_BRASS_BACKPACK = REGISTRY.register("light_blue_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> BLUE_BRASS_BACKPACK = REGISTRY.register("blue_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> PURPLE_BRASS_BACKPACK = REGISTRY.register("purple_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> MAGENTA_BRASS_BACKPACK = REGISTRY.register("magenta_brass_backpack", BrownBrassBackpackBlock::new);
-	public static final DeferredBlock<Block> PINK_BRASS_BACKPACK = REGISTRY.register("pink_brass_backpack", BrownBrassBackpackBlock::new);
 	// End of user code block custom blocks
 }
